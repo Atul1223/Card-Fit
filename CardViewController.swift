0dc3c0ab8cc48b8c0a4496dd31c1c39b69fb23af
Atul1223 <a1atulk@gmail.com>
2023-09-23 22:56:27 +0530

    Added timer and random card generating logic with functionality of buttons
    Signed-off-by: Atul1223 <a1atulk@gmail.com>

--- CardViewController.swift
+++ CardViewController.swift
@@ -14,22 +14,38 @@ class CardViewController: UIViewController {
     
     @IBOutlet var buttons: [UIButton]!
     
+    var timer: Timer!
+    
+    var cards : [UIImage] = Card.allValues
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startTimer()
         for button in buttons {
             button.layer.cornerRadius = 8
         }
 
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        timer.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-        
+        timer.invalidate()
     }
     
     @IBAction func restartButtonTapped(_ sender: Any) {
-    }
-    
-    @IBAction func rulesButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        startTimer()
     }
 }
